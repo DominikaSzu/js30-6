c77eca19990919f0d9d3d3423aacfd9c8205e7b8
Dominika Szukała <dominika.szukala@gmail.com>
2018-09-22 22:56:17 +0200

    Data fetched

--- js/app.js
+++ js/app.js
@@ -2,5 +2,13 @@ document.addEventListener('DOMContentLoaded', function(e) {
     
     const endpoint = 'https://gist.githubusercontent.com/erdem/8c7d26765831d0f9a8c62f02782ae00d/raw/248037cd701af0a4957cce340dabb0fd04e38f4c/countries.json'
     
+    const cities = [];
+    
+    fetch(endpoint)
+        .then(response => response.json())
+        .then(data => cities.push(data))
+        .catch(error => console.log(`Uups, this is the trouble maker: ${error}`));
+    
+    console.log(cities);
     
 });
