c66384fea7cc344fa264e47c8b4b859cf7fbe4a0
Dominika Szukała <dominika.szukala@gmail.com>
2018-09-22 21:53:51 +0200

    Gist of country and capitals added

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(e) {
-
+    
+    const endpoint = 'https://gist.githubusercontent.com/erdem/8c7d26765831d0f9a8c62f02782ae00d/raw/248037cd701af0a4957cce340dabb0fd04e38f4c/countries.json'
     
     
 });
