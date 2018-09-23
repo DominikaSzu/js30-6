b0979f6cfb2391d442f422516f30b902ac685de3
Dominika Szukała <dominika.szukala@gmail.com>
2018-09-23 18:13:27 +0200

    New gist added

--- js/app.js
+++ js/app.js
@@ -1,31 +1,30 @@
-document.addEventListener('DOMContentLoaded', function(e) {
-    
-    const endpoint = 'https://gist.githubusercontent.com/erdem/8c7d26765831d0f9a8c62f02782ae00d/raw/248037cd701af0a4957cce340dabb0fd04e38f4c/countries.json'
-    
+    const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
     const cities = [];
-    
-    function checkFetchStatus (response) {
+    const searchInput = document.querySelector('.search-input');
+
+    function checkFetchStatus(response) {
         if (response.status === 200) {
-            return Promise.resolve(response);
+            return Promise.resolve(response)
         } else {
             return Promise.reject(
-                new Error(response.statusText));
+            new Error(response.statusText));
         }
     }
-                
-    function turnToJson (data) {
+
+    function turnToJson(data) {
         return data.json();
     }
-    
-    
+
+
     fetch(endpoint)
         .then(checkFetchStatus)
         .then(turnToJson)
-        .then(data => cities.push(data))
-        .catch(error => console.log(`Uups, this is the trouble maker: ${error}`));
-    
-    console.log(cities);
-            
+        .then(data => cities.push(...data))
+        .catch(error => console.log(`Upss, that's the trobule reason: ${error}`))
 
-    
-});
+    function matchWord(word, cities) {
+        return cities.filter(city => {
+            let regex = new RegExp(word, 'gi');
+            return city.city.match(regex) || city.state.match(regex)
+        })
+    }
